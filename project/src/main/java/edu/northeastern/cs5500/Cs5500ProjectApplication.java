713c9ebbde93697b6b416b8cb898f531827c75d6
Praveen Kumar Singh <praveensingh@ccs.neu.edu>
2018-02-11 14:31:23 -0500

    setup project

---
+++ project/src/main/java/edu/northeastern/cs5500/Cs5500ProjectApplication.java
@@ -0,0 +1,12 @@
+package edu.northeastern.cs5500;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs5500ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs5500ProjectApplication.class, args);
+	}
+}
