934f0c26b4de1965e9e8f2b03691cf5f9dd842fa
Praveen Kumar Singh <praveensingh@ccs.neu.edu>
2018-03-18 19:07:57 -0400

    removed unused methods

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -6,61 +6,13 @@
     function userService($http) {
         var api = {
             "login" : login,
-            "logout": logout,
-            "findUserByCredentials": findUserByCredentials,
-            "findUserById": findUserById,
             "findByUserIdAndUserName" : findByUserIdAndUserName,
-            "createUser": register,
-            "updateUser": updateUser,
-            "deleteUser" : deleteUser,
-            "isAdmin" :isAdmin,
-            "updateProfile" :updateProfile,
-            "findUserByUserId": findUserByUserId,
-            "addUserFollowingList": addUserFollowingList,
-            "removeFollowerById": removeFollowerById,
-            "unFollowUserById": unFollowUserById,
-            "findAllUsers": findAllUsers,
-            "registerByAdmin": registerByAdmin
+            "createUser": register
         };
         return api;
 
-        function findAllUsers(){
-            return $http.get("/api/allUsers")
-                .then(function (response) {
-                    return response.data;
-                });
-        }
-
         function login(user) {
             return $http.post('/api/login', user)
-                .then(function (response) {
-                    return response.data;
-                });
-        }
-
-        function logout() {
-            return $http.post('/api/logout')
-                .then(function (response) {
-                    return response.data;
-                });
-        }
-
-        function isAdmin() {
-            return $http.post('/api/isAdmin')
-                .then(function (response) {
-                    return response.data;
-                });
-        }
-
-        function updateProfile(user) {
-            return $http.put('/api/updateProfile/' + user)
-                .then(function (response) {
-                    return response.data;
-                });
-        }
-
-        function deleteUser(userId) {
-            return $http.delete('/api/user/' + userId)
                 .then(function (response) {
                     return response.data;
                 });
@@ -73,23 +25,6 @@
                 });
         }
 
-        function registerByAdmin(user) {
-            return $http.post('/api/registerByAdmin', user)
-                .then(function (response) {
-                    return response.data;
-                });
-        }
-
-        function updateUser(userId, newUser) {
-            return $http.put("/api/user/"+userId, newUser);
-        }
-
-        function findUserById(userId) {
-            return $http.get("/api/user/"+userId)
-                .then(function (response) {
-                    return response.data;
-                });
-        }
 
         function findByUserIdAndUserName(userId){
             return $http({
@@ -99,38 +34,6 @@
             }).then(function (response) {
                 return response.data;
             });
-        }
-
-        function findUserByUserId(userId) {
-            return $http.get("/api/user/id/"+userId)
-                .then(function (response) {
-                    return response.data;
-                });
-        }
-
-        function findUserByCredentials(username, password) {
-            return $http.get("/api/user?username="+username+"&password="+password);
-        }
-
-        function addUserFollowingList(userId){
-            return $http.get("/api/user/follow/"+userId)
-                .then(function (response) {
-                    return response.data;
-                });
-        }
-
-        function removeFollowerById(userId){
-            return $http.get("/api/user/removeFollower/"+userId)
-                .then(function (response) {
-                    return response.data;
-                });
-        }
-
-        function unFollowUserById(userId) {
-            return $http.get("/api/user/unFollow/"+userId)
-                .then(function (response) {
-                    return response.data;
-                });
         }
 
     }
