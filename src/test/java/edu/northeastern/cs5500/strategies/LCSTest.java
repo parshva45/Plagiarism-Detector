01b8959ea1bcdc469a82c4712b0bb952a3361949
Praveen Kumar Singh <praveensingh@ccs.neu.edu>
2018-03-21 18:23:40 -0400

    updated

--- src/test/java/edu/northeastern/cs5500/strategies/LCSTest.java
+++ src/test/java/edu/northeastern/cs5500/strategies/LCSTest.java
@@ -20,17 +20,17 @@ public class LCSTest extends Cs5500PlagiarismDetectorTeam207ApplicationTests{
 	@Autowired
 	LCS lcs;
 
-	private String s1;
-	private String s2;
-	private String s3;
-	private String s4;
+	private String file1;
+	private String file2;
+	private String file3;
+	private String file4;
 
 	@Before
 	public void setUp() {
-		s1 = pythonToStringParser.readFile(getFilePath("submission1.py")).trim();
-		s2 = pythonToStringParser.readFile(getFilePath("submission2.py")).trim();
-		s3 = pythonToStringParser.readFile(getFilePath("submission3.py")).trim();
-		s4 = pythonToStringParser.readFile(getFilePath("submission4.py")).trim();
+		file1 = getFilePath("submission1.py");
+		file2 = getFilePath("submission2.py");
+		file3 = getFilePath("submission3.py");
+		file4 = getFilePath("submission4.py");
 	}
 
 	public String getFilePath(String fileName){
@@ -42,38 +42,37 @@ public class LCSTest extends Cs5500PlagiarismDetectorTeam207ApplicationTests{
 
 	@Test
 	public void compares1s2Test() {
-    	double similarityMeasure = lcs.calculateSimilarity(s1, s2);
+    	double similarityMeasure = lcs.calculateSimilarity(file1, file2);
     	assertEquals(90.338, similarityMeasure, 0.01);
 	}
 	
 	@Test
 	public void compares1s3Test() {
-		double similarityMeasure = lcs.calculateSimilarity(s1, s3);
+		double similarityMeasure = lcs.calculateSimilarity(file1, file3);
     	assertEquals(90.338, similarityMeasure, 0.01);
 	}
 	
 	@Test
 	public void compares1s4Test() {
-		double similarityMeasure = lcs.calculateSimilarity(s1, s4);
+		double similarityMeasure = lcs.calculateSimilarity(file1, file4);
     	assertEquals(86.175, similarityMeasure, 0.01);
 	}
 	
 	@Test
 	public void compares2s3Test() {
-		double similarityMeasure = lcs.calculateSimilarity(s2, s3);
+		double similarityMeasure = lcs.calculateSimilarity(file2, file3);
     	assertEquals(100.0, similarityMeasure, 0.01);
 	}
 	
 	@Test
 	public void compares2s4Test() {
-		double similarityMeasure = lcs.calculateSimilarity(s2, s4);
+		double similarityMeasure = lcs.calculateSimilarity(file2, file4);
     	assertEquals(95.392, similarityMeasure, 0.01);
 	}
 	
 	@Test
 	public void compares3s4Test() {
-    	double similarityMeasure = lcs.calculateSimilarity(
-				getFilePath("submission3.py"), getFilePath("submission4.py"));
+    	double similarityMeasure = lcs.calculateSimilarity(file3, file4);
     	assertEquals(95.392, similarityMeasure, 0.01);
 	}
 
