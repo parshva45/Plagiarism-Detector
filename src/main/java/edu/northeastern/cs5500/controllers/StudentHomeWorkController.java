a40adfc404f54c0886c43f60633765ba64a05d52
Praveen Kumar Singh <praveensingh@ccs.neu.edu>
2018-03-21 00:32:41 -0400

    updated

--- src/main/java/edu/northeastern/cs5500/controllers/StudentHomeWorkController.java
+++ src/main/java/edu/northeastern/cs5500/controllers/StudentHomeWorkController.java
@@ -2,10 +2,15 @@ package edu.northeastern.cs5500.controllers;
 
 import edu.northeastern.cs5500.model.StudentHomeWork;
 import edu.northeastern.cs5500.service.StudentHomeWorkService;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
 
@@ -13,6 +18,7 @@ import java.util.function.BiFunction;
  * @author Praveen Singh
  */
 @RestController
+@RequestMapping("/api/studentHomeWork")
 public class StudentHomeWorkController {
     private static final String ERROR_HEADER = "error";
 
@@ -23,8 +29,13 @@ public class StudentHomeWorkController {
         this.studentHomeWorkService = studentHomeWorkService;
     }
 
-
-
+    @RequestMapping(path = "/getHomeWorksForStudent/{studentId}", method = RequestMethod.GET)
+    public JSONObject getHomeWorksForStudent(@PathVariable int studentId){
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("result", studentHomeWorkService.getListOfHomeWorksByStudentId(studentId));
+        resultMap.put("response-code", "OK");
+        return new JSONObject(resultMap);
+    }
 
 
     private ResponseEntity<StudentHomeWork> catchRequestExceptions(final int userId,
--- src/main/java/edu/northeastern/cs5500/model/Course.java
+++ src/main/java/edu/northeastern/cs5500/model/Course.java
@@ -1,28 +1,24 @@
 package edu.northeastern.cs5500.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 /**
  * @author Praveen Singh
  */
+@Entity
+@Table(name = "course")
 public class Course implements Serializable{
     @Id
     @GeneratedValue
-    private int courseId;
+    private int id;
     private String name;
 
-    public int getCourseId() {
-        return courseId;
-    }
-
-    public void setCourseId(int courseId) {
-        this.courseId = courseId;
-    }
-
-    public Course withCourseId(Integer courseId){
-        this.courseId = courseId;
+    public Course withId(Integer id){
+        this.setId(id);
         return this;
     }
 
@@ -35,7 +31,15 @@ public class Course implements Serializable{
     }
 
     public Course withCourseName(String name){
-        this.name = name;
+        this.setName(name);
         return this;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
