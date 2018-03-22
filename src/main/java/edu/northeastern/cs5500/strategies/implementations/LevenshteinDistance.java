82727f3b77d24ec3175d51ee6204ec2a36fb768f
parshva45 <parshva45@gmail.com>
2018-03-22 15:44:42 -0400

    added javadocs for levenshtein distance strategy

--- src/main/java/edu/northeastern/cs5500/strategies/implementations/LevenshteinDistance.java
+++ src/main/java/edu/northeastern/cs5500/strategies/implementations/LevenshteinDistance.java
@@ -12,11 +12,19 @@ public class LevenshteinDistance implements SimilarityStrategy {
 
     private final PythonToStringParser pythonToStringParser;
 
+    /**
+     * @param pythonToStringParser
+     */
     @Autowired
     public LevenshteinDistance(PythonToStringParser pythonToStringParser) {
         this.pythonToStringParser = pythonToStringParser;
     }
 
+    /**
+     * @param compOne
+     * @param compTwo
+     * @return edit distance between strings compOne and compTwo using Levenshtein distance
+     */
     public int getDistance(String compOne, String compTwo) {
         int[][] matrix = new int[compOne.length() + 1][compTwo.length() + 1];
         for (int i = 0; i <= compOne.length(); i++) {
@@ -40,19 +48,37 @@ public class LevenshteinDistance implements SimilarityStrategy {
         return matrix[compOne.length()][compTwo.length()];
     }
  
+    /**
+     * @param first
+     * @param second
+     * @return cost of substitution between chars first and second
+     */
     private int costOfSubstitution(char first, char second) {
         return first == second ? 0 : 1;
     }
  
+    /**
+     * @param numbers
+     * @return number with minimum value if exists, max integer otherwise
+     */
     private int min(int... numbers) {
         return Arrays.stream(numbers).min().orElse(Integer.MAX_VALUE);
     }
     
+    /**
+     * @param s1
+     * @param s2
+     * @return longer of the two strings s1 and s2
+     */
     public String longer(String s1, String s2) {
     	return s1.length() >= s2.length() ? s1 : s2;
     }
 
 
+    /* (non-Javadoc)
+     * @see edu.northeastern.cs5500.strategies.SimilarityStrategy#calculateSimilarity(java.lang.String, java.lang.String)
+     * similarity measure between file1 and file2 using Levenshtein distance strategy
+     */
     @Override
     public double calculateSimilarity(String file1, String file2){
         String fileContentFile1 = pythonToStringParser.readFile(file1).trim();
