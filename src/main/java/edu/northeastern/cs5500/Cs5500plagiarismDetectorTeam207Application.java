56bbc6a47dc9a7893a170dc45bf0bd59f22729e3
Praveen Kumar Singh <praveensingh@ccs.neu.edu>
2018-03-22 18:54:13 -0400

    implemented log4j

--- src/main/java/edu/northeastern/cs5500/Cs5500plagiarismDetectorTeam207Application.java
+++ src/main/java/edu/northeastern/cs5500/Cs5500plagiarismDetectorTeam207Application.java
@@ -1,9 +1,7 @@
 package edu.northeastern.cs5500;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -11,28 +9,19 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class Cs5500plagiarismDetectorTeam207Application extends SpringBootServletInitializer
-		implements ApplicationRunner {
+public class Cs5500plagiarismDetectorTeam207Application extends SpringBootServletInitializer {
 	private static final Logger LOGGER =
 			LogManager.getLogger(Cs5500plagiarismDetectorTeam207Application.class);
 
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		LOGGER.info("Starting application");
 		return application.sources(Cs5500plagiarismDetectorTeam207Application.class);
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cs5500plagiarismDetectorTeam207Application.class, args);
-	}
-
-	@Override
-	public void run(ApplicationArguments applicationArguments) {
-		LOGGER.debug("Debugging log");
-		LOGGER.info("Info log");
-		LOGGER.warn("Hey, This is a warning!");
-		LOGGER.error("Oops! We have an Error. OK");
-		LOGGER.fatal("Damn! Fatal error. Please fix me.");
 	}
 
 }
--- src/main/java/edu/northeastern/cs5500/controllers/CourseController.java
+++ src/main/java/edu/northeastern/cs5500/controllers/CourseController.java
@@ -1,6 +1,8 @@
 package edu.northeastern.cs5500.controllers;
 
 import edu.northeastern.cs5500.service.CourseService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/course/")
 public class CourseController {
+    private static final Logger LOGGER = LogManager.getLogger(CourseController.class);
 
     private final CourseService courseService;
 
@@ -30,6 +33,7 @@ public class CourseController {
         Map<String, Object> resultMap = new HashMap<>();
         resultMap.put("result", courseService.getCourseById(courseId));
         resultMap.put("response-code", "OK");
+        LOGGER.info("sent JSONObject as API Response");
         return new JSONObject(resultMap);
     }
 }
