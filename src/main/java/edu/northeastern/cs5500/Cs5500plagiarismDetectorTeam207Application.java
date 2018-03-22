a71e2f6442dedcf02ff24efeeb063b77dc48b8b2
Praveen Kumar Singh <praveensingh@ccs.neu.edu>
2018-03-22 17:43:59 -0400

    adding logger

--- src/main/java/edu/northeastern/cs5500/Cs5500plagiarismDetectorTeam207Application.java
+++ src/main/java/edu/northeastern/cs5500/Cs5500plagiarismDetectorTeam207Application.java
@@ -1,12 +1,21 @@
 package edu.northeastern.cs5500;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+
 @SpringBootApplication
-public class Cs5500plagiarismDetectorTeam207Application extends SpringBootServletInitializer {
+public class Cs5500plagiarismDetectorTeam207Application extends SpringBootServletInitializer
+		implements ApplicationRunner {
+	private static final Logger LOGGER =
+			LogManager.getLogger(Cs5500plagiarismDetectorTeam207Application.class);
+
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -15,6 +24,15 @@ public class Cs5500plagiarismDetectorTeam207Application extends SpringBootServle
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cs5500plagiarismDetectorTeam207Application.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments applicationArguments) {
+		LOGGER.debug("Debugging log");
+		LOGGER.info("Info log");
+		LOGGER.warn("Hey, This is a warning!");
+		LOGGER.error("Oops! We have an Error. OK");
+		LOGGER.fatal("Damn! Fatal error. Please fix me.");
 	}
 
 }
