774c03ff3334549ccfe24eb07c44f8a871e1dfb4
Namrata Bilurkar <bilurkar.n@husky.neu.edu>
2018-04-06 16:19:07 -0400

    Tree edit distance calculation

--- src/main/java/edu/northeastern/cs5500/strategies/implementations/ast/treeeditdistance/TreeEditDistance.java
+++ src/main/java/edu/northeastern/cs5500/strategies/implementations/ast/treeeditdistance/TreeEditDistance.java
@@ -1,4 +1,54 @@
 package edu.northeastern.cs5500.strategies.implementations.ast.treeeditdistance;
 
-public class TreeEditDistance {
+import edu.northeastern.cs5500.strategies.SimilarityStrategy;
+import edu.northeastern.cs5500.strategies.implementations.ast.lcs.LongestCommonSubSequence;
+import edu.northeastern.cs5500.strategies.implementations.ast.pythonast.AstBuilder;
+import edu.northeastern.cs5500.strategies.implementations.ast.pythonast.ParserFacade;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TreeEditDistance implements SimilarityStrategy {
+
+    private static final Logger LOGGER = LogManager.getLogger(LongestCommonSubSequence.class);
+
+    @Autowired
+    AstBuilder astBuilder;
+
+    @Autowired
+    ParserFacade parserFacade;
+
+
+    @Autowired
+    public TreeEditDistance(AstBuilder astBuilder, ParserFacade parserFacade) {
+        this.astBuilder = astBuilder;
+        this.parserFacade = parserFacade;
+    }
+
+    @Override
+    public double calculateSimilarity(String file1, String file2) {
+
+        String ast1, ast2;
+        try {
+            ast1 = astBuilder.build(parserFacade.parse(new File(file1)));
+            ast2 = astBuilder.build(parserFacade.parse(new File(file2)));
+
+            Tree tree1 = new Tree(ast1);
+            Tree tree2 = new Tree(ast2);
+
+            int ast1Length = ast1.length();
+            int ast2Length = ast2.length();
+
+            int treeEditDistance = Tree.zhangShasha(tree1, tree2);
+            return (((double) treeEditDistance / Math.max(ast1Length, ast2Length)) * 100);
+
+        } catch (IOException e) {
+            LOGGER.error("Failed to get Similarity for input file {} and {}", file1, file2);
+        }
+
+        return 0.0;
+    }
 }
