0176340bb54b841f0f85f20dea68a993cdf5682f
Praveen Kumar Singh <praveensingh@ccs.neu.edu>
2018-04-07 15:46:12 -0400

    commented swagger

--- src/main/java/edu/northeastern/cs5500/config/SwaggerConfig.java
+++ src/main/java/edu/northeastern/cs5500/config/SwaggerConfig.java
@@ -1,28 +1,28 @@
-package edu.northeastern.cs5500.config;
-
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-/**
-* @author Praveen Singh
-* Swagger Config File, for loading all the API's in swagger
-*/
-@Configuration
-@EnableSwagger2
-@EnableAutoConfiguration
-public class SwaggerConfig {
-   @Bean
-   public Docket api() {
-       return new Docket(DocumentationType.SWAGGER_2)
-               .select()
-               .apis(RequestHandlerSelectors.any())
-               .paths(PathSelectors.any())
-               .build();
-   }
-}
+//package edu.northeastern.cs5500.config;
+//
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+///**
+//* @author Praveen Singh
+//* Swagger Config File, for loading all the API's in swagger
+//*/
+//@Configuration
+//@EnableSwagger2
+//@EnableAutoConfiguration
+//public class SwaggerConfig {
+//   @Bean
+//   public Docket api() {
+//       return new Docket(DocumentationType.SWAGGER_2)
+//               .select()
+//               .apis(RequestHandlerSelectors.any())
+//               .paths(PathSelectors.any())
+//               .build();
+//   }
+//}
