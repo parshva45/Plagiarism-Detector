e4d3511ebd7087a62a43c378c0c27e4d64af8002
Praveen Kumar Singh <praveensingh@ccs.neu.edu>
2018-04-14 14:47:15 -0400

    all

---
+++ src/main/java/edu/northeastern/cs5500/service/CompareWithAllAsyncService.java
@@ -0,0 +1,15 @@
+package edu.northeastern.cs5500.service;
+
+/**
+ * @author Praveen Singh
+ */
+public class CompareWithAllAsyncService {
+
+    /**
+     * Method compares all the submissions against all other submissions and
+     * sends an email for plagiarism.
+     */
+    public void compareAllForPlagiarism(){
+
+    }
+}
